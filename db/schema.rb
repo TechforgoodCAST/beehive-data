454846bdd0313f6d67ee1357c763e4bee723360f
David Kane <d.r.kane@gmail.com>
2017-03-22 11:58:52 +0000

    Add Charity Commission area of operation mappings to fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322093328) do
+ActiveRecord::Schema.define(version: 20170322114723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20170322093328) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.json     "altnames"
+    t.string   "ccaoo"
   end
 
   add_index "countries", ["alpha2"], name: "index_countries_on_alpha2", using: :btree
